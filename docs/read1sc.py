884494afd7570657a5a80e2d040bac37f343cdbe
Matt Clapp <itsayellow@gmail.com>
2017-08-25 14:13:09 -0400

    more cleanup

--- docs/read1sc.py
+++ docs/read1sc.py
@@ -8,6 +8,11 @@ import os.path
 import struct
 
 """
+Directly cited by field_type=16
+    Need format of type=1000
+    Need format of type=1007
+    Need format of type=1022
+
 NOTES:
 --------
 Header
@@ -111,44 +116,73 @@ test.1sc:
     image data 59946 - 783785 (last byte of file)
 
 """
-def print_list(byte_list, bits=8, dec_not_hex=True):
+def print_list(byte_list, bits=8, dec_not_hex=True, address=None):
+    """
+    TODO: is this doing proper little-endian?
+    """
     # log10(2**bits) = log10(2)*bits = 0.301*bits
     # log16(2**bits) = log16(2)*bits = 0.25*bits
     hex_digits = int(0.25 * bits)
     # add 3 chars for "0x" and ","
+    # number of items in a line
     items = 72//(hex_digits+3)
-    # get
+    # round items down to multiple of 4
     items = items//4*4
-    if dec_not_hex:
-        pr_str = "{:%dd},"%(hex_digits+2)
-    else:
-        pr_str = "0x{:0%dx},"%(hex_digits)
+    pr_str = "{:%dd},"%(hex_digits+2)
+    pr_str_hex = "0x{:0%dx},"%(hex_digits)
 
     byte_groups = range(0, len(byte_list), items)
     byte_groups = [[x, min([x+items, len(byte_list)])] for x in byte_groups]
 
-    print("\t[", end="")
-    first_loop = True
-    for (i,byte_group) in enumerate(byte_groups):
-        if first_loop:
-            first_loop=False
-        else:
-            print("\t ", end="")
+    if address is None:
+        print("\t[", end="")
+        first_loop = True
+        for (i,byte_group) in enumerate(byte_groups):
+            if first_loop:
+                first_loop=False
+            else:
+                print("\t ", end="")
 
-        for byte in byte_list[byte_group[0]:byte_group[1]]:
-            print(pr_str.format(byte), end="")
-
-        if i<len(byte_groups)-1:
+            # print decimal words
+            for byte in byte_list[byte_group[0]:byte_group[1]]:
+                print(pr_str.format(byte), end="")
+            # print spacer
             print()
-    print("]")
+            print("         ", end="")
+            # print hex words
+            for byte in byte_list[byte_group[0]:byte_group[1]]:
+                print(pr_str_hex.format(byte), end="")
+
+            if i<len(byte_groups)-1:
+                print()
+        print("]")
+    else:
+        first_loop = True
+        for (i,byte_group) in enumerate(byte_groups):
+            # print address start
+            if len(byte_groups) > 1:
+                print("    %6d: "%(address+i*items*bits/8), end="")
+            else:
+                print("            ", end="")
+            # print decimal words
+            for byte in byte_list[byte_group[0]:byte_group[1]]:
+                print(pr_str.format(byte), end="")
+            print()
+
+            # print spacer
+            print("            ", end="")
+            # print hex words
+            for byte in byte_list[byte_group[0]:byte_group[1]]:
+                print(pr_str_hex.format(byte), end="")
+            print()
 
 
 def str_safe_bytes(byte_stream):
     table_from = bytes(range(256))
     table_to = b'\x20' + b'\xff'*31 + bytes(range(32,127)) + b'\xff'*129
     trans_table = byte_stream.maketrans( table_from, table_to )
-    out_byte_stream = byte_stream.translate(trans_table)
-    return out_byte_stream
+    safe_byte_stream = byte_stream.translate(trans_table)
+    return safe_byte_stream
     
 
 def debug_generic(byte_stream, byte_start, note_str, format_str, quiet=False):
@@ -159,8 +193,12 @@ def debug_generic(byte_stream, byte_start, note_str, format_str, quiet=False):
     if not quiet:
         print("%6d-%6d"%(byte_start,byte_idx-1), end="")
         print("\t"+note_str+":")
-        print_list(out_shorts, bits=bytes_per*8)
-        print_list(out_shorts, bits=bytes_per*8, dec_not_hex=False)
+        print_list(
+                out_shorts,
+                bits=bytes_per*8,
+                address=byte_start
+                )
+        print()
     return (out_shorts, byte_idx)
 
 
@@ -231,8 +269,10 @@ def is_valid_string(byte_stream):
 
 
 def read_field(in_bytes, byte_idx, note_str="??", field_data={}):
-    # 0     Jump Field - nop filler data (or used somehow?)
-    # 2     nop field - payload is all 0's
+    # 0     Jump Field - nop filler data
+    #       Around data block boundaries
+    #       Contains info about data block, at end and beginning of data block
+    # 2     nop field - payload is all 0's, otherwise normal header
 
     # 16    String field - text label assigned to previous data through data_id
     #       Last 4 bytes of field header of this field is data_id that matches
@@ -241,15 +281,33 @@ def read_field(in_bytes, byte_idx, note_str="??", field_data={}):
     # 100   Data field - contains multiple data assigned to future text labels
     #       Last 4 bytes of field headers of field_type=16 is data_id that match
     #       data_id uints in this field payload
-    # 101   Data field - contains multiple data assigned to future text
-    #       4 uint string matches data from field_type=100 assigned to
-    #       field_type=16 text label
-    # 102   Data field - contains multiple data assigned to future text
-    #       4 uint string matches data from field_type=100 assigned to
-    #       field_type=16 text label
+    #       Every 36 bytes is data item
+    #       Bytes 12-15 are uint data_id tag
+    # 101   Data field - contains multiple data assigned to future text labels
+    #       Last 4 bytes of field headers of field_type=16 is data_id that match
+    #       data_id uints in this field payload
+    #       Every 20 bytes is data item
+    #       Bytes 16-19 are uint data_id tag
+    # 102   Data field - contains multiple data assigned to future text labels
+    #       Last 4 bytes of field headers of field_type=16 is data_id that match
+    #       data_id uints in this field payload
+    #       Every 16 bytes is data item
+    #       Bytes 12-15 are uint data_id tag
     # 131   Data field - contains multiple data assigned to future text labels
     #       Last 4 bytes of field headers of field_type=16 is data_id that match
     #       data_id uints in this field payload
+    #       Every 12 bytes is data item
+    #       Bytes 4-7 are uint data_id tag
+    # 1000  Not fully understood - Irregular data block
+    #       Sometimes for field_type= 16, sometimes not (??)
+    #       Is format fixed based on which data block?
+    # 1007  Not fully understood - Irregular data block
+    #       Is format fixed based on which data block?
+    # 1022  No data items, only data_id tags?
+    #       4 uints in payload, first 3 uints are data_id tags
+    #       Every 4 bytes is data item, last 4 bytes are not used (??)
+    #       Bytes 0-3 are uint data_id tag
+
 
     # 126   Data Block 6
     #       1st uint a pointer to byte val=6180 4 uints before end of Jump Field,
@@ -312,15 +370,13 @@ def read_field(in_bytes, byte_idx, note_str="??", field_data={}):
     #       uint[0] = data block start, uint[1] = data length
     #       this starts after end of field_type=142's data block
 
-    # 1000
+    # Not understood yet:
     # 1004
-    # 1007
     # 1008
     # 1010
     # 1011
     # 1015
     # 1020
-    # 1022
     # 1024
     # 1030
     # 1040
@@ -450,7 +506,7 @@ def read_field_generic(in_bytes, byte_idx, note_str="??"):
 
 
 def read_field_type0(in_bytes, byte_idx, note_str="??"):
-    # EXPERIMENTAL ALGORITHM FOR FINDING END OF JUMP
+    # Finding the end of the jump algorithm
     # READ:
     #   ushort field_type=0
     #   ushort 8
@@ -554,8 +610,7 @@ def read_field_type16(in_bytes, byte_idx, note_str="??", field_data={}):
                 field_payload, byte_idx+8, "bytes")
     if field_len !=0:
         if data_tag in field_data:
-            print_list(field_data[data_tag], bits=32, dec_not_hex=True)
-            print_list(field_data[data_tag], bits=32, dec_not_hex=False)
+            print_list(field_data[data_tag], bits=32)
         else:
             print("DATA NOT FOUND")
 
@@ -824,6 +879,7 @@ def read_field_type131(in_bytes, byte_idx, note_str="??", field_data={}):
 
 
 def read_field_type1000(in_bytes, byte_idx, note_str="??", field_data={}):
+    # TODO: extract data for future field_type=16
     field_info = {}
     print("---------------------------------------------------------------")
     print("byte_idx = "+repr(byte_idx))
@@ -873,7 +929,8 @@ def read_field_type1000(in_bytes, byte_idx, note_str="??", field_data={}):
     return (byte_idx+field_len, field_info)
 
 
-def read_field_type1022(in_bytes, byte_idx, note_str="??", field_data={}):
+def read_field_type1007(in_bytes, byte_idx, note_str="??", field_data={}):
+    # TODO: extract data for future field_type=16
     field_info = {}
     print("---------------------------------------------------------------")
     print("byte_idx = "+repr(byte_idx))
@@ -915,7 +972,7 @@ def read_field_type1022(in_bytes, byte_idx, note_str="??", field_data={}):
     return (byte_idx+field_len, field_info)
 
 
-def read_field_type1007(in_bytes, byte_idx, note_str="??", field_data={}):
+def read_field_type1022(in_bytes, byte_idx, note_str="??", field_data={}):
     field_info = {}
     print("---------------------------------------------------------------")
     print("byte_idx = "+repr(byte_idx))
@@ -947,9 +1004,10 @@ def read_field_type1007(in_bytes, byte_idx, note_str="??", field_data={}):
         (out_ints, _) = debug_ints(
                 field_payload, byte_idx+8, "ints")
 
-    # TODO: what is the format of this??
-    #for i in range(len(out_uints)//4):
-    #    field_data[out_uints[i*4+3]] = out_uints[i*4:i*4+4]
+    # first three uints are data_id tags, no associated data
+    field_data[out_uints[0]] = []
+    field_data[out_uints[1]] = []
+    field_data[out_uints[2]] = []
 
     field_info['type'] = field_type
     field_info['payload'] = field_payload
@@ -1047,31 +1105,31 @@ byte_idx = 160
 # field_data is data from last field_type=100 field, to be used in
 #   following field_type=16 fields
 field_data = {}
-img_data_idx_start = 0xffffffff
+img_data_start = 0xffffffff
 while( byte_idx < len(in_bytes) ):
     field_start = byte_idx
     (byte_idx, field_info) = read_field(in_bytes, byte_idx, field_data=field_data)
     if 'data' in field_info:
         field_data = field_info['data']
     if field_info['type']==127:
-        (data07_idx_start, data07_len) = parse_datablock(field_info['payload'])
+        (data07_start, data07_len) = parse_datablock(field_info['payload'])
         print("Field Type 127 - Data Block 07 (???)")
-        print("    data starts at byte %d"%(data07_idx_start))
+        print("    data starts at byte %d"%(data07_start))
         print("    data length is %d bytes"%(data07_len))
     if field_info['type']==128:
-        (data08_idx_start, data08_len) = parse_datablock(field_info['payload'])
+        (data08_start, data08_len) = parse_datablock(field_info['payload'])
         print("Field Type 128 - Data Block 08 (???)")
-        print("    data starts at byte %d"%(data08_idx_start))
+        print("    data starts at byte %d"%(data08_start))
         print("    data length is %d bytes"%(data08_len))
     if field_info['type']==129:
-        (data09_idx_start, data09_len) = parse_datablock(field_info['payload'])
+        (data09_start, data09_len) = parse_datablock(field_info['payload'])
         print("Field Type 129 - Data Block 09 (???)")
-        print("    data starts at byte %d"%(data09_idx_start))
+        print("    data starts at byte %d"%(data09_start))
         print("    data length is %d bytes"%(data09_len))
     if field_info['type']==130:
-        (img_data_idx_start, img_data_len) = parse_datablock(field_info['payload'])
+        (img_data_start, img_data_len) = parse_datablock(field_info['payload'])
         print("Field Type 130 - Data Block 10 (Image Data)")
-        print("    data starts at byte %d"%(img_data_idx_start))
+        print("    data starts at byte %d"%(img_data_start))
         print("    data length is %d bytes"%(img_data_len))
 
     # break if we still aren't advancing
@@ -1080,31 +1138,37 @@ while( byte_idx < len(in_bytes) ):
         print("--------------------------------------------------------------")
         break
 
-    if byte_idx > img_data_idx_start:
+    if byte_idx > img_data_start:
         print("--------------------------------------------------------------")
-        print("We passed the img data, so BREAK!!")
+        print("We passed the start of img data, so BREAK!!")
         print("--------------------------------------------------------------")
         break
 
 # parse data blocks
 
 # Data Block 7
-print_datablock(data07_idx_start, data07_len, "7")
+print_datablock(data07_start, data07_len, "7")
 
 # Data Block 8
-print_datablock(data08_idx_start, data08_len, "8")
+print_datablock(data08_start, data08_len, "8")
 
 # Data Block 9
-print_datablock(data09_idx_start, data09_len, "9")
+print_datablock(data09_start, data09_len, "9")
 
 # Data Block 10 - Image Data
-#print_datablock(img_data_idx_start, img_data_len, "10")
+print("=====================================================================")
+print("IMAGE DATA BLOCK")
+print()
+#print_datablock(img_data_start, img_data_len, "10")
 
-# byte_idx_start = img_data_idx_start
-# byte_idx_end = img_data_idx_start + img_data_len
+img_data_end = img_data_start + img_data_len
+print("Image Data: (%d-%d)"%(img_data_start,img_data_end-1))
 #(img_ushorts, _) = debug_ushorts(
-#        in_bytes[byte_idx_start:byte_idx_end],
-#        byte_idx_start, "img_data")
-#
+#        in_bytes[img_data_start:img_data_end],
+#        img_data_start, "img_data")
+
 #for img_ushort in img_ushorts:
 #    print(img_ushort)
+
+print()
+print()
