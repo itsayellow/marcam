3a5477442910769d0141ad175a93f282126e028e
Matt Clapp <itsayellow@gmail.com>
2017-10-26 13:03:53 -0700

    mark only yellow dot on image

--- cellcounter/const.py
+++ cellcounter/const.py
@@ -20,11 +20,12 @@ DEBUG = DEBUG_KEYPRESS | DEBUG_TIMING | DEBUG_MISC
 # useful for bitmaps below
 pix_clear = b'\x00\x00\x00\x00'
 pix_red = b'\xff\x00\x00\xff'
+pix_yellow = b'\xff\xff\x00\xff'
 
 # red cross, 5px x 5px
 centerline_5 = pix_clear*2 + pix_red + pix_clear*2
 crossline_5 = pix_red*5
-CROSS_BMP_5x5 = wx.Bitmap.FromBufferRGBA(
+CROSS_5x5_BMP = wx.Bitmap.FromBufferRGBA(
         5, 5,
         centerline_5*2 + crossline_5 + centerline_5*2
         )
@@ -32,7 +33,7 @@ CROSS_BMP_5x5 = wx.Bitmap.FromBufferRGBA(
 # red cross, 7px x 7px
 centerline_7 = pix_clear*3 + pix_red + pix_clear*3
 crossline_7 = pix_red*7
-CROSS_BMP_7x7 = wx.Bitmap.FromBufferRGBA(
+CROSS_7x7_BMP = wx.Bitmap.FromBufferRGBA(
         7, 7,
         centerline_7*3 + crossline_7 + centerline_7*3
         )
@@ -40,7 +41,7 @@ CROSS_BMP_7x7 = wx.Bitmap.FromBufferRGBA(
 # red cross, 9px x 9px
 centerline_9 = pix_clear*4 + pix_red + pix_clear*4
 crossline_9 = pix_red*9
-CROSS_BMP_9x9 = wx.Bitmap.FromBufferRGBA(
+CROSS_9x9_BMP = wx.Bitmap.FromBufferRGBA(
         9, 9,
         centerline_9*4 + crossline_9 + centerline_9*4
         )
@@ -48,7 +49,9 @@ CROSS_BMP_9x9 = wx.Bitmap.FromBufferRGBA(
 # red cross, 11px x 11px
 centerline_11 = pix_clear*5 + pix_red + pix_clear*5
 crossline_11 = pix_red*11
-CROSS_BMP_11x11 = wx.Bitmap.FromBufferRGBA(
+CROSS_11x11_BMP = wx.Bitmap.FromBufferRGBA(
         11, 11,
         centerline_11*5 + crossline_11 + centerline_11*5
         )
+
+YELLOW_PIX_BMP = wx.Bitmap.FromBufferRGBA(1, 1, pix_yellow)
