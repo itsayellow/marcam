5bf35e8b1a92d9953061fc70bb2dfe14540496f6
Matt Clapp <itsayellow+dev@gmail.com>
2017-11-03 01:07:29 -0700

    making setup.py cross-platform to enable py2exe

--- setup.py
+++ setup.py
@@ -5,69 +5,93 @@ Usage:
     python3 setup.py py2app
 """
 
+import sys
 from setuptools import setup
 
-APP = ['cellcounter/cellcounter.py']
-APP_NAME = 'Cellcounter'
-VERSION = '0.1.0'
-DATA_FILES = [
-        'cellcounter/topen32.png',
-        'cellcounter/marktool32.png',
-        'cellcounter/cellcounter_help.html',
-        'cellcounter/cellcounter_help_markmodeoff.png'
-        ]
-OPTIONS = {
-        'argv_emulation':True,
-        'iconfile':'cellcounter/cellcounter.icns',
-        'optimize':1, # try one level of optimization?
-        'plist':{
-            'CFBundleName':APP_NAME,
-            'CFBundleDisplayName':APP_NAME,
-            'CFBundleGetInfoString':'Count cells in biological images',
-            'CFBundleIdentifier':'com.itsayellow.osx.cellcounter',
-            'CFBundleVersion':VERSION,
-            'CFBundleShortVersionString':VERSION,
-            'NSHumanReadableCopyright': u"Copyright © 2017, " \
-                    "Matthew A. Clapp, All Rights Reserved",
-            'CFBundleDocumentTypes':[
-                {
-                    'CFBundleTypeName':'Bio-Rad Gel File',
-                    'CFBundleTypeRole':'Viewer',
-                    #'CFBundleTypeIconFile':'.icns',
-                    'LSHandlerRank': "Alternate",
-                    'LSItemContentTypes': ["com.itsayellow.1sc"],
-                    },
-                {
-                    'CFBundleTypeName':'TIFF Image',
-                    'CFBundleTypeRole':'Viewer',
-                    'LSHandlerRank': "Alternate",
-                    'LSItemContentTypes': ["public.tiff"],
-                    },
-                {
-                    'CFBundleTypeName':'PNG Image',
-                    'CFBundleTypeRole':'Viewer',
-                    'LSHandlerRank': "Alternate",
-                    'LSItemContentTypes': ["public.png"],
-                    },
-                {
-                    'CFBundleTypeName':'JPEG Image',
-                    'CFBundleTypeRole':'Viewer',
-                    'LSHandlerRank': "Alternate",
-                    'LSItemContentTypes': ["public.jpeg"],
-                    },
-                ],
-            'UTExportedTypeDeclarations': [{
-                'UTTypeConformsTo': ["public.data"],
-                'UTTypeIdentifier': "com.itsayellow.1sc",
-                'UTTypeDescription': "Bio-Rad Gel File",
-                'UTTypeTagSpecification': {'public.filename-extension': "1sc"}
-            }],
-            },
-        }
+
+# global
+mainscript = 'cellcounter/cellcounter.py'
+app_name = 'Cellcounter'
+
+if sys.platform == 'darwin':
+    # Mac and py2app
+    version = '0.1.0'
+    data_files = [
+            'cellcounter/topen32.png',
+            'cellcounter/marktool32.png',
+            'cellcounter/cellcounter_help.html',
+            'cellcounter/cellcounter_help_markmodeoff.png'
+            ]
+    py2app_options = {
+            'argv_emulation':True,
+            'iconfile':'cellcounter/cellcounter.icns',
+            'optimize':1, # try one level of optimization?
+            'plist':{
+                'CFBundleName':app_name,
+                'CFBundleDisplayName':app_name,
+                'CFBundleGetInfoString':'Count cells in biological images',
+                'CFBundleIdentifier':'com.itsayellow.osx.cellcounter',
+                'CFBundleVersion':version,
+                'CFBundleShortVersionString':version,
+                'NSHumanReadableCopyright': u"Copyright © 2017, " \
+                        "Matthew A. Clapp, All Rights Reserved",
+                'CFBundleDocumentTypes':[
+                    {
+                        'CFBundleTypeName':'Bio-Rad Gel File',
+                        'CFBundleTypeRole':'Viewer',
+                        #'CFBundleTypeIconFile':'.icns',
+                        'LSHandlerRank': "Alternate",
+                        'LSItemContentTypes': ["com.itsayellow.1sc"],
+                        },
+                    {
+                        'CFBundleTypeName':'TIFF Image',
+                        'CFBundleTypeRole':'Viewer',
+                        'LSHandlerRank': "Alternate",
+                        'LSItemContentTypes': ["public.tiff"],
+                        },
+                    {
+                        'CFBundleTypeName':'PNG Image',
+                        'CFBundleTypeRole':'Viewer',
+                        'LSHandlerRank': "Alternate",
+                        'LSItemContentTypes': ["public.png"],
+                        },
+                    {
+                        'CFBundleTypeName':'JPEG Image',
+                        'CFBundleTypeRole':'Viewer',
+                        'LSHandlerRank': "Alternate",
+                        'LSItemContentTypes': ["public.jpeg"],
+                        },
+                    ],
+                'UTExportedTypeDeclarations': [{
+                    'UTTypeConformsTo': ["public.data"],
+                    'UTTypeIdentifier': "com.itsayellow.1sc",
+                    'UTTypeDescription': "Bio-Rad Gel File",
+                    'UTTypeTagSpecification': {'public.filename-extension': "1sc"}
+                }],
+                },
+            }
+    extra_options = dict(
+            app=[mainscript],
+            data_files=data_files,
+            options={'py2app': py2app_options},
+            setup_requires=['py2app']
+            )
+elif sys.platform == 'win32':
+    # Windows and py2exe
+    # win32 is name of platform even on 64-bit Windows!
+    extra_options = dict(
+            app=[mainscript],
+            #data_files=DATA_FILES,
+            #options={'py2exe': OPTIONS},
+            setup_requires=['py2exe']
+            )
+else:
+    # for unix-like platforms to use setup.py install to install main script
+    extra_options = dict(
+            scripts=[mainscript],
+            )
 
 setup(
-    app=APP,
-    data_files=DATA_FILES,
-    options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
+        name=app_name,
+        **extra_options
 )
