5fa39c9b5375e7ae73992b7bd344457987950bf6
Matt Clapp <itsayellow+dev@gmail.com>
2018-02-10 22:57:59 -0800

    redo prev. if drag rectsize is (1,1), return don't pass

--- cellcounter/image_scrolled_canvas.py
+++ cellcounter/image_scrolled_canvas.py
@@ -370,17 +370,6 @@ class ImageScrolledCanvas(wx.ScrolledCanvas):
             return
 
         if evt.Dragging() and evt.LeftIsDown():
-            # NOTE: Yosemite VM always says a click is a drag.  Does non-VM?
-            # only set self.is_dragging flag if draw_rect is ever not (1,1)
-            #   (1,1) means start point and end point the same (i.e. click)
-            # Once set, only on_left_up can unset self.is_dragging
-            if draw_rect.GetSize() != (1, 1):
-                self.is_dragging = True
-            else:
-                return
-
-            # TODO: if self.mouse_left_down['point_unscroll'] is near a point
-            #   then move that point, don't drag a box
             evt_pos = evt.GetPosition()
             evt_pos_unscroll = self.CalcUnscrolledPosition(evt_pos)
 
@@ -400,6 +389,15 @@ class ImageScrolledCanvas(wx.ScrolledCanvas):
                 return
             except Exception as exc:
                 raise exc
+
+            # NOTE: Yosemite VM always says a click is a drag.  Does non-VM?
+            # only set self.is_dragging flag if draw_rect is ever not (1,1)
+            #   (1,1) means start point and end point the same (i.e. click)
+            # Once set, only on_left_up can unset self.is_dragging
+            if draw_rect.GetSize() != (1, 1):
+                self.is_dragging = True
+            else:
+                return
 
             # make copy of rects, inflate by 1 pixel in each dir, union
             #   inflate by same width as rubberband rect Pen width
@@ -1469,7 +1467,7 @@ class ImageScrolledCanvasMarks(ImageScrolledCanvas):
             if draw_rect.GetSize() != (1, 1):
                 self.is_dragging = True
             else:
-                pass
+                return
                 # DEBUG DELETEME
                 #LOGGER.warning("Drag with (1,1) size")
 
