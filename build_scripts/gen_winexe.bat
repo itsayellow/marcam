6d7392eb204c0b241cc6cdfaa03360f0efd7fac9
Matthew A. Clapp <itsayellow+dev@gmail.com>
2018-03-07 22:32:29 -0800

    package html help imgs too

--- build_scripts/gen_winexe.bat
+++ build_scripts/gen_winexe.bat
@@ -4,4 +4,6 @@ pyinstaller ^
     --add-data marcam/marktool32.png;media ^
     --add-data marcam/pointer32.png;media ^
     --add-data marcam/marcam_help.html;media ^
+    --add-data marcam/help_markmode_off.png;media ^
+    --add-data marcam/help_selectmode_off.png;media ^
     marcam/marcam.py
