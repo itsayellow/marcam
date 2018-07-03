5d6dbb362fe350fa94142106656afcdba68123f0
Matthew Clapp <itsayellow+dev@gmail.com>
2018-07-03 12:04:01 -0700

    adding Apache 2.0 boilerplate

--- marcam/common.py
+++ marcam/common.py
@@ -1,5 +1,18 @@
 """Common functions used by multiple modules
 """
+# Copyright 2017-2018 Matthew A. Clapp
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 def debug_fxn_factory(logger_fxn):
     """Factory to produce debug_fxn that logs to specified logger object
