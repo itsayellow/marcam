1809a8e839f8c068704d87a0615c7e4f9dbe79f5
Matthew Clapp <itsayellow+dev@gmail.com>
2018-09-03 22:32:05 -0700

    Use better way of finding testdata dir.

--- tests/test_mcmfile.py
+++ tests/test_mcmfile.py
@@ -3,7 +3,11 @@
 import mcmfile
 import pathlib
 
-TESTDATA_PATH = pathlib.Path.cwd() / '..' / 'tests' / 'testdata'
+# the parent of this file is the tests directory
+TESTS_PATH = pathlib.Path(__file__).resolve().parent
+# path to testdata dir
+TESTDATA_PATH = TESTS_PATH / 'testdata'
+
 def test_is_valid():
     mcm_testfile = TESTDATA_PATH / 'checkerboard.mcm'
     assert mcmfile.is_valid(mcm_testfile) is True
