ce1732b686ce8b26b2220abeb582ae2320e17aa8
Matthew Clapp <itsayellow+dev@gmail.com>
2018-09-05 21:07:05 -0700

    Make legacy and 1.0 mcm is_valid and load.

--- tests/test_mcmfile.py
+++ tests/test_mcmfile.py
@@ -8,22 +8,25 @@ TESTS_PATH = pathlib.Path(__file__).resolve().parent
 # path to testdata dir
 TESTDATA_PATH = TESTS_PATH / 'testdata'
 
-def test_is_valid():
-    mcm_testfile = TESTDATA_PATH / 'checkerboard.mcm'
-    assert mcmfile.is_valid(mcm_testfile) is True
+LEGACY_MCM_1SC_FILE = TESTDATA_PATH / 'legacy_mcm_1sc_A11 2015-12-15 11hr 55min.mcm'
+MCM_1_0_FILE = TESTDATA_PATH / 'single_pixel_lines.mcm'
+
+def test_is_valid_legacy():
+    assert mcmfile.is_valid(LEGACY_MCM_1SC_FILE) is True
+
+def test_is_valid_mcm_1_0_file():
+    assert mcmfile.is_valid(MCM_1_0_FILE) is True
 
 def test_load_legacy_mcm_file():
     # legacy mcm file
-    legacy_mcm_1sc_testfile = TESTDATA_PATH / 'legacy_mcm_1sc_A11 2015-12-15 11hr 55min.mcm'
-    (wx_image, marks, img_name) = mcmfile.load(legacy_mcm_1sc_testfile)
+    (wx_image, marks, img_name) = mcmfile.load(LEGACY_MCM_1SC_FILE)
     assert wx_image.IsOk()
     assert isinstance(marks, list)
     assert isinstance(img_name, str)
 
 def test_load_mcm_1_0_file():
     # legacy mcm file
-    mcm_testfile = TESTDATA_PATH / 'single_pixel_lines.mcm'
-    (wx_image, marks, img_name) = mcmfile.load(mcm_testfile)
+    (wx_image, marks, img_name) = mcmfile.load(MCM_1_0_FILE)
     assert wx_image.IsOk()
     assert isinstance(marks, list)
     assert isinstance(img_name, str)
