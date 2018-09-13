6b0d93add1d3e765fcf18025c647b457b06e7bf4
Matthew Clapp <itsayellow+dev@gmail.com>
2018-09-13 14:10:18 -0700

    Inherit ThreadedProgressPulse from Threaded.

--- marcam/longtask.py
+++ marcam/longtask.py
@@ -108,7 +108,7 @@ class Threaded:
         self.post_thread_fxn(*self.thread_fxn_returnvals)
 
 
-class ThreadedProgressPulse:
+class ThreadedProgressPulse(Threaded):
     """Class supporting long tasks that need to be in separate thread.
 
     Handles running the thread part of the task in a separate thread,
@@ -119,33 +119,9 @@ class ThreadedProgressPulse:
     @debug_fxn
     def __init__(self, thread_fxn, thread_fxn_args, post_thread_fxn,
             progress_title, progress_msg, parent):
-        """Initialize a Long Task needing thread execution and wx support.
-
-        Args:
-            thread_fxn (function handle): long-running function to be run in
-                separate thread.  Return values from this function will
-                be passed as positional arguments to post_thread_fxn.
-            thread_fxn_args (tuple): arguments for thread_fxn
-            post_thread_fxn (function handle): function that runs after
-                thread_fxn has finished
-            progress_title (str): Text for titlebar of wx.ProgressDialog
-            progress_msg (str): Text for message area of wx.ProgressDialog
-            parent (wx.Window): Window that handles events and is parent
-                of ProgressDialog
-        """
-
-        self.thread_fxn = thread_fxn
-        self.thread_fxn_args = thread_fxn_args
-        self.post_thread_fxn = post_thread_fxn
         self.win_parent = parent
         self.thread_fxn_returnvals = None
-        # get new Event and EventBinder for this instance only
-        (self.myLongTaskDoneEvent, evt_long_task_done) = wx.lib.newevent.NewEvent()
 
-        task_thread = threading.Thread(
-                target=self.long_task_thread,
-                )
-        self.win_parent.Bind(evt_long_task_done, self.long_task_postthread)
         self.progress_dialog = wx.ProgressDialog(
                 progress_title,
                 progress_msg,
@@ -154,32 +130,10 @@ class ThreadedProgressPulse:
         # Pulse seems to only be needed to be called once!  Not multiple times
         #   as the docs imply.
         self.progress_dialog.Pulse()
-        # Start task thread computing.
-        # Do this last, so that if it ends super fast we are not trying to
-        #   still do things with self.progress_dialog after long_task_postthread
-        #   Destroys the dialog.
-        task_thread.start()
-
-    @debug_fxn
-    def long_task_thread(self):
-        """Function that is run in separate thread
-
-        If thread_fxn returns any values, they will be passed as positional
-        arguments to post_thread_fxn.
-        """
-        thread_fxn_returnvals = self.thread_fxn(*self.thread_fxn_args)
-        if thread_fxn_returnvals is None:
-            # if returnvals = None, make empty tuple
-            self.thread_fxn_returnvals = ()
-        else:
-            try:
-                # if returnvals are iterable, convert to tuple
-                self.thread_fxn_returnvals = tuple(thread_fxn_returnvals)
-            except TypeError:
-                # if returnvals are single value, wrap in tuple
-                self.thread_fxn_returnvals = (thread_fxn_returnvals,)
-
-        wx.PostEvent(self.win_parent, self.myLongTaskDoneEvent())
+        # invoke thread stuff after setting up progress_dialog, so thread
+        #   ending and post-thread destroying progress_dialog is impossible
+        #   to come first
+        super().__init__(thread_fxn, thread_fxn_args, post_thread_fxn, parent)
 
     @debug_fxn
     def long_task_postthread(self, _evt):
@@ -193,4 +147,4 @@ class ThreadedProgressPulse:
         #   to continue
         self.progress_dialog.Destroy()
         # execute post thread function with return value(s) from thread_fxn
-        self.post_thread_fxn(*self.thread_fxn_returnvals)
+        super().long_task_postthread(_evt)
